40995156a6a16d1f213644ccc892516cd36f6a9d
Thomas Menari <thomas.menari@sohonet.com>
2018-07-27 10:50:22 -0700

    add username support for brocade_netiron enable

--- netmiko/brocade/brocade_netiron.py
+++ netmiko/brocade/brocade_netiron.py
@@ -1,11 +1,33 @@
 from __future__ import unicode_literals
+import re
 from netmiko.cisco_base_connection import CiscoSSHConnection
+from netmiko.ssh_exception import NetMikoTimeoutException
 
 
 class BrocadeNetironBase(CiscoSSHConnection):
     def save_config(self, cmd='write memory', confirm=False):
         """Save Config"""
         return super(BrocadeNetironBase, self).save_config(cmd=cmd, confirm=confirm)
+
+    def enable(self, cmd='enable', pattern='(user name|password):', re_flags=re.IGNORECASE):
+        output = ""
+        msg = "Failed to enter enable mode. Please ensure you pass " \
+              "the 'secret' argument to ConnectHandler."
+        if not self.check_enable_mode():
+            self.write_channel(self.normalize_cmd(cmd))
+            try:
+                output += self.read_until_prompt_or_pattern(pattern=pattern, re_flags=re_flags)
+                if 'User Name' in output:
+                    self.write_channel(self.normalize_cmd(self.username))
+                    self.write_channel(self.normalize_cmd(self.secret))
+                else:
+                    self.write_channel(self.normalize_cmd(self.secret))
+                output += self.read_until_prompt()
+            except NetMikoTimeoutException:
+                raise ValueError(msg)
+            if not self.check_enable_mode():
+                raise ValueError(msg)
+        return output
 
 
 class BrocadeNetironSSH(BrocadeNetironBase):
